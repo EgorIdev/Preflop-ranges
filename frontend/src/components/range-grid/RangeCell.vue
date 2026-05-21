dc11859852bc911a70db4cbcd7b22344730c7e73
MentalEdge <isupovegor777@gmail.com>
2026-05-21 12:13:40 +0700

    add 13x13 matrix

---
+++ frontend/src/components/range-grid/RangeCell.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps<{
+  hand: string
+}>()
+</script>
+
+<template>
+
+  <div
+    class="
+      aspect-square
+      bg-zinc-800
+      rounded
+      flex
+      items-center
+      justify-center
+      text-sm
+      font-semibold
+      cursor-pointer
+      hover:bg-zinc-700
+      transition
+    "
+  >
+    {{ hand }}
+  </div>
+
+</template>
---
+++ frontend/src/components/range-grid/RangeGrid.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { generateGrid } from '../../utils/poker'
+
+import RangeCell from './RangeCell.vue'
+
+const grid = generateGrid()
+</script>
+
+<template>
+
+  <div
+    class="
+      grid
+      grid-cols-13
+      gap-1
+    "
+  >
+
+    <template
+      v-for="row in grid"
+      :key="row[0]"
+    >
+
+      <RangeCell
+        v-for="hand in row"
+        :key="hand"
+        :hand="hand"
+      />
+
+    </template>
+
+  </div>
+
+</template>
